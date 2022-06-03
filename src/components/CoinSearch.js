498b47aa61bb15dc07e297bee6afe76f5d81bf76
edomos <alf.ramoz@gmail.com>
2022-06-03 00:24:57 -0700

    create jsx for <CoinSearch /> and <CoinItem />

---
+++ src/components/CoinSearch.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { AiOutlineStar } from 'react-icons/ai';
+import Coinitem from './CoinItem';
+
+const CoinSearch = ({ coins }) => {
+    return (
+        <div>
+            <div>
+                <h1>Search Crypto</h1>
+                <form>
+                    <input type="text" placeholder="Search a coin"/>
+                </form>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>#</th>
+                        <th>Coin</th>
+                        <th></th>
+                        <th>Price</th>
+                        <th>24h</th>
+                        <th>24h Volume</th>
+                        <th>Mkt</th>
+                        <th>Last 7 Days</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {coins.map((coin) => (
+                       <Coinitem key={coin.id} coin={coin}/>
+                    ))}
+                </tbody>
+            </table>
+
+        </div>
+
+        
+
+    );
+}
+
+export default CoinSearch;
