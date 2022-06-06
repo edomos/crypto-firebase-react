9a7698afa8a137ecc43750707960932f67a49d0f
edomos <alf.ramoz@gmail.com>
2022-06-06 01:23:11 -0700

    create footer component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Signup from "./routes/Signup"
 import Account from "./routes/Account"
 import axios from "axios";
 import CoinPage from "./routes/CoinPage";
+import Footer from "./components/Footer";
 
 function App() {
 
@@ -35,6 +36,7 @@ function App() {
           <Route path=":coinId"/>
         </Route>
       </Routes>
+      <Footer />
     </ThemeProvider>
     </BrowserRouter>
   );
