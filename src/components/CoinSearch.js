2f8f65133662704ddb3a99b48ba8d399f32f78e4
edomos <alf.ramoz@gmail.com>
2022-06-07 22:08:51 -0700

    add optional chaining

--- src/components/CoinSearch.js
+++ src/components/CoinSearch.js
@@ -12,7 +12,7 @@ const CoinSearch = ({ coins }) => {
         <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
           <h1 className="text-2xl font-bold my-2">Search Crypto</h1>
           <form>
-            <input
+            <inputtw
               className="w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl"
               onChange={(event) => setSearchText(event.target.value)}
               type="text"
